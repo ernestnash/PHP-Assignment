76f15d95715676c5b5d76cb636bc5dbddc27d903
Nashon Ernest <92583323+ernestnash@users.noreply.github.com>
2023-09-26 08:43:26 +0300

    Update api.php with comments

--- api.php
+++ api.php
@@ -1,14 +1,16 @@
 <?php
+// api.php
 
-// fetch JSON data from url and return the decoded data as an array
+// Fetch JSON data from the provided URL and return the decoded data as an array
 function getJSONData($json_url) {
     $json_data = file_get_contents($json_url);
 
     $data = json_decode($json_data, true);
 
+    // Check if there was an error while decoding the JSON data
     if ($data === null) {
         return ['error' => 'Failed to decode JSON data. Error: ' . json_last_error_msg()];
     }
-    return $data;
-    }
+    return $data; // Return the decoded data
+}
 ?>
